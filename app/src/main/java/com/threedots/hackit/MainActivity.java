994484bf5c3695c62e5abfe5b3a1d096d010060f
Sohaib <sohaibbinmusa@gmail.com>
2020-08-28 22:16:04 +0600

    Added Fragments

--- app/src/main/java/com/threedots/hackit/MainActivity.java
+++ app/src/main/java/com/threedots/hackit/MainActivity.java
@@ -1,14 +1,85 @@
 package com.threedots.hackit;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private HackathonFragment hackathonFragment;
+    private ContestFragment contestFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        hackathonFragment = new HackathonFragment();
+        contestFragment = new ContestFragment();
+
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        tabLayout.setupWithViewPager(viewPager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+
+        viewPagerAdapter.addFragment(hackathonFragment, "Hackathon");
+        viewPagerAdapter.addFragment(contestFragment, "Contest");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+        private List <String> fragmentTitle = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager fm, int behaviour) {
+            super(fm , behaviour);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragments.add(fragment);
+            fragmentTitle.add(title);
+        }
+
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitle.get(position);
+        }
     }
 }
